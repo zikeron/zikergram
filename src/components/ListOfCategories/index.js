44db359a862abbd6db1640ba4d8d5bfbb60f9cf0
Alejandro cortez <alejandro.cortes@ironbit.com.mx>
2020-03-26 22:41:16 -0600

    Inserted data from db.json

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Category } from '../Category'
 import { Item, List } from './styles'
+import { categories } from '../../../api/db'
 
 export const ListOfCategories = () => {
   return (
     <List>
       {
-        [1, 2, 3, 4].map(category => <Item key={category}><Category /></Item>)
+        categories.map(category => (
+          <Item key={category.id}>
+            <Category
+              {...category}
+            />
+          </Item>)
+        )
       }
     </List>
   )
