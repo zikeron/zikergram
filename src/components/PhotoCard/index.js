63f019b2b362d3038d1eebf54d8051ae5d3bec9b
Alejandro cortez <alejandro.cortes@ironbit.com.mx>
2020-03-28 23:41:50 -0600

    Separated custom hooks behaviour

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,47 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Img, Button, ImageWrapper, Article } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage} from '../../Hooks/useLocalStorage'
+import { useNearScreen} from '../../Hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const ref = useRef(null)
-  const [show, setShow] = useState(false)
+  const [show, ref] = useNearScreen()
   const key = `like-${id}`
-  const [liked, setLiked] = useState(() => {
-    try {
-      const value = window.localStorage.getItem(key)
-      return (value === 'true')
-    } catch (e) {
-      console.error(e)
-    }
-  })
+  const [liked, setLiked] = useLocalStorage(key, false)
   const Icon = liked ? MdFavorite : MdFavoriteBorder
-  const setLocalStorage = value => {
-    try {
-      window.localStorage.setItem(key, value)
-      setLiked(value)
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  useEffect(() => {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(ref.current)
-    })
-  }, [ref])
 
   return (
     <Article ref={ref}>
@@ -53,7 +22,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                 <Img src={src} alt='Cover image' />
               </ImageWrapper>
             </a>
-            <Button onClick={() => setLocalStorage(!liked)}>
+            <Button onClick={() => setLiked(!liked)}>
               <Icon size='32px' />{likes} likes!
             </Button>
           </>
