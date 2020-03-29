11c092974f601cfc65085d546fd5bea0afb984f4
Alejandro cortez <alejandro.cortes@ironbit.com.mx>
2020-03-28 19:29:01 -0600

    Adding fetching dat from API

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { Item, List } from './styles'
-import { categories } from '../../../api/db'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    window.fetch('https://zikergram-api.now.sh/categories')
+      .then(json => json.json())
+      .then(response => setCategories(response))
+  }, [])
   return (
     <List>
       {
