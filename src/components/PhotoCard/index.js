69cd13a01b16153634a28a627ec251425d16202f
Alejandro cortez <alejandro.cortes@ironbit.com.mx>
2020-03-28 22:07:08 -0600

    Adding Intersection obvserver

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,18 +1,42 @@
-import React from 'react'
-import { Img, Button, ImageWrapper} from './styles'
+import React, { useEffect, useRef, useState } from 'react'
+import { Img, Button, ImageWrapper, Article } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => (
-  <article>
-    <a href={`/detail/${id}`}>
-      <ImageWrapper>
-        <Img src={src} alt='Cover image' />
-      </ImageWrapper>
-    </a>
-    <Button>
-      <MdFavoriteBorder size='32px' />{likes} likes!
-    </Button>
-  </article>
-)
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const ref = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    // console.log(ref.current)
+    const observer = new window.IntersectionObserver(function (entries) {
+      console.log(entries)
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        console.log('si')
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(ref.current)
+  }, [ref])
+
+  return (
+    <Article ref={ref}>
+      {
+        show &&
+          <>
+            <a href={`/detail/${id}`}>
+              <ImageWrapper>
+                <Img src={src} alt='Cover image' />
+              </ImageWrapper>
+            </a>
+            <Button>
+              <MdFavoriteBorder size='32px' />{likes} likes!
+            </Button>
+          </>
+      }
+    </Article>
+  )
+}
