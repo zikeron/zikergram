ee138efa471d51fad478dd5787cff37821f70989
Alejandro cortez <alejandro.cortes@ironbit.com.mx>
2020-03-28 19:52:47 -0600

    aded fixed list in scroll event

--- src/components/Category/styles.js
+++ src/components/Category/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { fadeIn } from '../../styles/animations';
 
 export const Anchor = styled.a`
   display: flex;
@@ -9,6 +10,7 @@ export const Anchor = styled.a`
 `
 
 export const Image = styled.img`
+  ${fadeIn()}
   border: 1px solid #ddd;
   box-shadow: 0px 10px 14px rgba(0, 0, 0, .2);
   border-radius: 50%;
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,13 +4,25 @@ import { Item, List } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] =  useState(false)
+
   useEffect(() => {
     window.fetch('https://zikergram-api.now.sh/categories')
       .then(json => json.json())
       .then(response => setCategories(response))
   }, [])
-  return (
-    <List>
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('onscroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => (
           <Item key={category.id}>
@@ -21,5 +33,13 @@ export const ListOfCategories = () => {
         )
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+
   )
 }
