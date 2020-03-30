b13b674ef1c6e76dcdd26ef79ffa86a00a6efe6c
Alejandro cortez <alejandro.cortes@ironbit.com.mx>
2020-03-29 19:41:42 -0600

    Added mitation to login user

--- src/pages/Unregistered.js
+++ src/pages/Unregistered.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
+import { LoginMutation } from '../container/LoginMutation'
 
 export const Unregistered = () => {
   return (
@@ -25,7 +26,21 @@ export const Unregistered = () => {
                   }
                 }
               </RegisterMutation>
-              <UserForm onSubmit={activateAuth} title='Inicia Sesión' />
+              <LoginMutation>
+                {
+                  (login, { data, loading, error }) => {
+                    const onSubmit = ({ email, password }) => {
+                      const input = { email, password }
+                      const variables = { input }
+                      login({ variables }).then(activateAuth)
+                    }
+
+                    const errorMsg = error && 'The password is not correct or the user does not exist'
+
+                    return <UserForm disabled={loading} error={errorMsg} title='Iniciar sesión' onSubmit={onSubmit} />
+                  }
+                }
+              </LoginMutation>
             </>
           )
         }
