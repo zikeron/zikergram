f9be16c7c1fbf00588146b75221dfcf29c7f9268
Alejandro cortez <alejandro.cortes@ironbit.com.mx>
2020-03-29 20:13:28 -0600

    Integrated authorization by jwt

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Img, Button, ImageWrapper, Article } from './styles'
-import { useLocalStorage } from '../../Hooks/useLocalStorage'
+import { Img, ImageWrapper, Article } from './styles'
 import { useNearScreen } from '../../Hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
@@ -8,10 +7,8 @@ import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, ref] = useNearScreen()
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
 
   return (
     <Article ref={ref}>
@@ -27,8 +24,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               {
                 (toggleLike) => {
                   const handleFavClick = () => {
-                    !liked && toggleLike({ variables: { input: { id } } })
-                    setLiked(!liked)
+                    toggleLike({ variables: { input: { id } } })
                   }
                   return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
                 }
